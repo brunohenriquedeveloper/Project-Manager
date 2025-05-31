0f84ff2dbfa44d4d43f5d9dee10cb5b40318f2ae
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-05-31 10:53:20 -0300

    Feat: Implemented the Navbar and footer

--- Project-Manager/src/App.jsx
+++ Project-Manager/src/App.jsx
@@ -3,27 +3,26 @@ import Home from "./components/pages/Home"
 import Contact from "./components/pages/Contact"
 import Company from "./components/pages/Company"
 import NewProject from "./components/pages/NewProject"
-
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Projects from './components/pages/Projects'
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/contact">Contato</Link> |{" "}
-        <Link to="/company">Empresa</Link> |{" "}
-        <Link to="/newproject">Novo Projeto</Link>
-      </nav>
+      <Navbar />
      <Container customClass="min-height">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/company" element={<Company />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="/newproject" element={<NewProject />} />
         
       </Routes>
     </Container>
+    <Footer />
     </Router>
   )
 }
---
+++ Project-Manager/src/components/layout/Navbar.jsx
@@ -0,0 +1,37 @@
+import {Link} from 'react-router-dom'
+
+import Container from './Container'
+
+import styles from './Navbar.module.css'
+
+import logo from '../../img/Logo.png'
+
+function Navbar(){
+    return (
+        <nav className={styles.navbar}>
+        <Container>
+        <Link to="/"><img src={logo} alt="TaskFlow" srcset="" /></Link>
+
+    <ul className={styles.list}>
+        <li>
+        <Link to="/" className={styles.item}>Home</Link>
+        </li>
+
+         <li>
+        <Link to="/projects" className={styles.item}>Projects</Link>
+        </li>
+
+        <li>
+        <Link to="/contact" className={styles.item}>Contato</Link>
+        </li>
+
+        <li><Link to="/company" className={styles.item}>Empresa</Link>
+        </li>
+
+        </ul>
+        </Container>
+        </nav>
+    )
+}
+
+export default Navbar
