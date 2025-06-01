25d694f945665f3f78ddaa6d7d3322907622d32d
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-06-01 18:43:36 -0300

    Feat: starting implementing the backend

--- Project-Manager/src/components/project/ProjectForm.jsx
+++ Project-Manager/src/components/project/ProjectForm.jsx
@@ -1,9 +1,27 @@
+import {useEffect, useState} from 'react'
+
 import styles from "./ProjectForm.module.css"
 import Input from "../form/input"
 import Select from "../form/Select"
 import Submit from "../form/Submit"
 
 function ProjectForm({btnText}){
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((resp) => resp.json()).then((data) => {
+        setCategories(data)
+    }).catch((err) => console.log(err))
+    }, [])
+
+    
+
     return (
         <form className={styles.form}>
            <Input
@@ -22,7 +40,7 @@ function ProjectForm({btnText}){
 
            />
 
-            <Select name = "category_id" text="Selecione a categoria"/>
+            <Select name = "category_id" text="Selecione a categoria" options={categories}/>
 
             <Submit text = {btnText}/>
             
