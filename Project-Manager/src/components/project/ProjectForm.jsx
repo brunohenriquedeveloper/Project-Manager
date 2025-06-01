a66ada40c757b7fd050901603bf382323d0add09
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-06-01 15:49:13 -0300

    Feat: Implemented the form

--- Project-Manager/src/components/project/ProjectForm.jsx
+++ Project-Manager/src/components/project/ProjectForm.jsx
@@ -1,21 +1,30 @@
-function ProjectForm(){
+import styles from "./ProjectForm.module.css"
+import Input from "../form/input"
+import Select from "../form/Select"
+import Submit from "../form/Submit"
+
+function ProjectForm({btnText}){
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do projeto" />
-            </div>
+        <form className={styles.form}>
+           <Input
+            type="text" 
+           text="Nome do Projeto" 
+           name="name"
+           placeholder="Insira o nome do Projeto"
 
-            <div>
-                <input type="number" placeholder="Insira o orçamento total" />
-            </div>
-            
-            <div>
-                <select name="category_id" id=""><option disabled>Selecione a Categoria</option></select>
-            </div>
+           />
 
-            <div>
-                <input type="submit" value="Criar Projeto"/>
-            </div>
+            <Input
+            type="number" 
+           text="Orçamento do Projeto" 
+           name="budget"
+           placeholder="Insira o orçamento total"
+
+           />
+
+            <Select name = "category_id" text="Selecione a categoria"/>
+
+            <Submit text = {btnText}/>
             
         </form>
     )
