64acd7e4ba603c148c44dc32d4d7f9e0f696f349
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-06-03 22:53:46 -0300

    Feat: Implemented the remove functionality

--- Project-Manager/src/components/pages/Projects.jsx
+++ Project-Manager/src/components/pages/Projects.jsx
@@ -11,6 +11,7 @@ function Projects(){
 
     const [projects, setProjects] = useState([])
     const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation()
     let message = ''
@@ -38,6 +39,22 @@ function Projects(){
         
     }, [])
 
+    function removeProject(id) {
+
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            },
+        }).then(resp => resp.json())
+        .then(data => {
+            setProjects(projects.filter((project) => project.id !== id))
+            setProjectMessage('Projeto removido com sucesso!')
+        })
+        .catch(err => console.log(err))
+
+    }
+
     return ( 
     <div className={styles.project_container}>
        
@@ -47,6 +64,7 @@ function Projects(){
             <a href="#">novo projeto</a>
             </div>
             {message && <Message type="success" msg={message}/>}
+            {projectMessage && <Message type="success" msg={projectMessage}/>}
             <Container customClass="start">
             {projects.length > 0 && projects.map((project) => {
               return(<ProjectCard 
@@ -55,6 +73,7 @@ function Projects(){
                 budget={project.budget}
                 category={project.category.name}
                 key={project.id}
+                handleRemove={removeProject}
                 />)  
             })}
             {!removeLoading && <Loading/>}
