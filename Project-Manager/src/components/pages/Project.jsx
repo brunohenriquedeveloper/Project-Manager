ed96867b6388e28485808a8b58a1741aa31848cc
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-06-05 10:55:32 -0300

    Feat: Initializing implementing the service custom system

--- Project-Manager/src/components/pages/Project.jsx
+++ Project-Manager/src/components/pages/Project.jsx
@@ -14,6 +14,7 @@ function Project() {
 
     const [project, setProject] = useState({})
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const[type, setType] = useState()
 
@@ -33,6 +34,9 @@ function Project() {
     }, [id])
 
     function editPost(project){
+        setMessage('')
+
+
         if(project.budget < project.cost){
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
             setType('error')
@@ -57,6 +61,10 @@ function Project() {
 
     function toggleProjectForm(){ 
         setShowProjectForm(!showProjectForm)
+    }
+
+      function toggleServiceForm(){ 
+        setShowServiceForm(!showServiceForm)
     }
 
     return (
@@ -92,6 +100,24 @@ function Project() {
                 </div>
             )}
         </div>
+
+        <div className={styles.service_form_container}>
+            <h2>Adicione um serviço:</h2>
+            <button className={styles.btn} onClick={toggleServiceForm}>
+                {!showServiceForm? 'Adicionar Serviço' : 'Fechar'}
+
+            </button>
+            <div className={styles.project_info}>
+            {
+                showServiceForm && <div>formulário do serviço</div>
+                
+            }
+            </div>
+        </div>
+        <h2>Serviços</h2>
+        <Container customClass="start">
+            <p>Itens de serviços</p>
+        </Container>
         </Container>
 
        </div>): <Loading />}
