6702843f75eac97d019b1b3e7eccf27a009a76ac
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-06-10 21:18:35 -0300

    Feat: Create a responsible code for the contact page

--- Project-Manager/src/components/pages/Contact.jsx
+++ Project-Manager/src/components/pages/Contact.jsx
@@ -1,5 +1,47 @@
-function Home(){
-    return <h1>Contact</h1>
+import Submit from "../form/Submit"
+import styles from "./Contact.module.css"
+import Input from "../form/input"
+
+function Contact(){
+
+    function handleChange(e){
+        setProject({...project, [e.target.name]: e.target.value })
+    }
+
+
+    return (
+        <div className={styles.conteiner_form}>
+        <form onSubmit={Submit} className={styles.contact_form}>
+            <h2>Entre em Contato:</h2>
+              <Input
+            type="text" 
+           name="name"
+           placeholder="Insira o seu nome"
+           handleOnChange={handleChange}
+
+           />
+
+           <Input
+            type="email" 
+           name="name"
+           placeholder="Insira o seu E-mail"
+           handleOnChange={handleChange}
+
+           />
+
+           <Input
+            type="text" 
+           name="name"
+           placeholder="Sua Mensagem"
+           handleOnChange={handleChange}
+
+           />
+           
+            <Submit text = "Enviar"/>
+        </form>
+        
+        </div>
+    )
 }
 
-export default Home
+export default Contact
