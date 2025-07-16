3798fbf421a833079c218b917535882081933277
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-07-15 22:50:06 -0300

    Feat: Implemented the data aspects of the company page

--- Project-Manager/src/components/pages/Company.jsx
+++ Project-Manager/src/components/pages/Company.jsx
@@ -7,7 +7,7 @@ function Company(){
 
             <div className={styles.leftCompany}>
             <h4>Onde tudo começou</h4>
-            <h2>Tornando um Mundo Caótico Mais Organizado!</h2>
+            <h2>Tornando o Mundo Mais Organizado!</h2>
             <p>Em 2023, um grupo de desenvolvedores apaixonados por tecnologia decidiu criar uma plataforma inovadora para gerenciar projetos de forma eficiente e colaborativa. Com o objetivo de simplificar a gestão de tarefas e melhorar a comunicação entre equipes, nasceu o Project Manager.</p>
             </div>
 
@@ -18,23 +18,23 @@ function Company(){
 
             <div className={styles.DataCompany}>
 
-            <div>
+            <div className={styles.databox}>
             <h3>2.5</h3>
             <h4>Anos de Experiência</h4>
             </div>
 
-            <div>
+            <div className={styles.databox}>
             <h3>+17k</h3>
             <h4>Projetos Gerenciados</h4>
             </div>
 
             
-            <div>
+            <div className={styles.databox}>
             <h3>+150</h3>
             <h4>Avaliações Positivas</h4>
             </div>
 
-            <div>
+            <div className={styles.databox}>
             <h3>+1.630</h3>
             <h4>Usuários Ativos</h4>
             </div>
